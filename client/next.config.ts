ff9be521a886940042837732700b0ba60cc83ecf
Muhammad Usman <112246337+Usman1361@users.noreply.github.com>
2024-11-16 13:36:56 +0500

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    TOGETHER_API_KEY: process.env.TOGETHER_API_KEY,
+  },
 };
 
 export default nextConfig;
